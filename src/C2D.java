9b1cf742a612e0dd437768feed3d00552ac40a8f
Shams Ansari <shamsahmedansari@gmail.com>
2021-01-17 20:38:35 -0800

    Added Comparable

--- src/C2D.java
+++ src/C2D.java
@@ -1,4 +1,19 @@
 public class C2D extends USD{
     private double convFactor = 1.36;
 
+    public C2D(){
+
+    }
+    public C2D(int dollars, int cents, String name, double convFactor){
+        super(dollars, cents, name);
+        this.convFactor = convFactor;
+    }
+
+    public double getConvFactor() {
+        return convFactor;
+    }
+
+    public void setConvFactor(double convFactor) {
+        this.convFactor = convFactor;
+    }
 }
--- src/USD.java
+++ src/USD.java
@@ -1,25 +1,54 @@
-public class USD {
+public class USD implements Comparable<USD>{
+
     private int dollars;
     private int cents;
     private String name;
 
-    public USD(){
+    public USD() {
 
     }
 
-    public USD(int dollars, int cents, String name){
+    public USD(int dollars, int cents, String name) {
         setDollars(dollars);
         setCents(cents);
         setName(name);
     }
 
-    public USD(USD copy){
+    public USD(USD copy) {
         setDollars(copy.getDollars());
         setCents(copy.getCents());
         setName(copy.getName());
     }
 
-    public
+    public void add(USD curr) {
+        if (!isSameCurrency(curr)) {
+            return;
+        }
+        int cents = (getCents() + curr.getCents()) % 100;
+        int dollars = getDollars() + curr.getDollars() + (getCents() + curr.getCents()) / 100;
+        setDollars(dollars);
+        setCents(cents);
+    }
+
+    public void sub(USD curr) {
+        if (!isSameCurrency(curr)) {
+           return;
+        }
+
+        int myTotal = getTotalInCents();
+        int currTotal = curr.getTotalInCents();
+        if (myTotal >= currTotal) {
+            int newVal = myTotal - currTotal;
+            setCents(newVal % 100);
+            setDollars(newVal / 100);
+        }
+    }
+
+
+    public boolean equals(USD curr) {
+        return isSameCurrency(curr) && getDollars() == curr.getDollars() && getCents() == curr.getCents();
+    }
+
 
 
 
@@ -45,5 +74,31 @@ public class USD {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+
+
+    protected int getTotalInCents() {
+        return getCents() + getDollars() * 100;
+    }
+    protected boolean isSameCurrency(USD curr){
+        return getName().equals(curr.getName());
+    }
+
+    @Override
+    public int compareTo(USD curr) {
+        if (!isSameCurrency(curr) || getTotalInCents() < curr.getTotalInCents()) {
+            return -1;
+        }
+        if(getTotalInCents() > curr.getTotalInCents()){
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
     }
 }
